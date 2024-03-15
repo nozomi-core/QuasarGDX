702fee1b7e67b3e4633b367d2ea2b342085f7ffc
PhoenixShell <app.phoenixshell@gmail.com>
2024-03-15 20:33:35 +1100

    Add kotlin compile to script

---
+++ tools/src/Example.kt
@@ -0,0 +1,5 @@
+class Example {
+    fun runThis() {
+        System.out.println("Heya")
+    }
+}
--- tools/src/MainRun.java
+++ tools/src/MainRun.java
@@ -1,6 +1,10 @@
 public class MainRun {
 
     public static void main(String[] args) {
+
+        Example pop = new Example();
+        pop.runThis();
+
         System.out.println("Hello App");
     }
 }
