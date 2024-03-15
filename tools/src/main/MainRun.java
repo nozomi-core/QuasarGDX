4f2557821d4392ba82628abe1e4d60ab8ef398fa
PhoenixShell <app.phoenixshell@gmail.com>
2024-03-15 21:01:12 +1100

    attempt to create test sources

--- tools/src/main/MainRun.java
+++ tools/src/main/MainRun.java
@@ -1,9 +1,11 @@
+package main;
+
 public class MainRun {
 
     public static void main(String[] args) {
 
         Example pop = new Example();
-        pop.runThis();
+       // pop.runThis();
 
         System.out.println("Hello App");
     }
