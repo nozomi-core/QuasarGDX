6b668ac6e31c45805d778f9330c195db535fcd2e
PhoenixShell <app.phoenixshell@gmail.com>
2024-03-15 21:28:14 +1100

    Add testing sources

---
+++ tools/src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+
+public class MainTest {
+    public static void main(String[] args) {
+
+        Example pop = new Example();
+        pop.runThis();
+
+        System.out.println("Hello Test");
+    }
+}
