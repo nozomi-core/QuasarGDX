1be5ccfc710cdce81b905099076460bcb7a3728f
PhoenixShell <app.phoenixshell@gmail.com>
2024-04-25 13:57:20 +1000

    Remove old code

--- quasar/src/main/java/app/quasar/qgl/language/GameData.kt
+++
@@ -1,7 +0,0 @@
-package app.quasar.qgl.language
-
-import app.quasar.qgl.engine.core.EngineApi
-
-abstract class GameData(engineApi: EngineApi) {
-    val runtimeId: Long = engineApi.generateId()
-}
