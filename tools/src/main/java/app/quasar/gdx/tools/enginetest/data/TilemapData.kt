0d1c6cd5e909fee6105f187692849b176c717246
PhoenixShell <app.phoenixshell@gmail.com>
2024-05-25 21:14:21 +1000

    Draft of serialization

--- tools/src/main/java/app/quasar/gdx/tools/enginetest/data/TilemapData.kt
+++ tools/src/main/java/app/quasar/gdx/tools/enginetest/data/TilemapData.kt
@@ -1,11 +1,12 @@
 package app.quasar.gdx.tools.enginetest.data
 
 import app.quasar.gdx.tools.model.TileInfo
+import app.quasar.gdx.tools.model.createRandomTileInfo
 import app.quasar.qgl.engine.core.GameData
 import app.quasar.qgl.serialize.BinProp
 import app.quasar.qgl.serialize.QGLEntity
 
 @QGLEntity("tilemap_data")
 class TilemapData {
-       var tiles: MutableList<TileInfo> = mutableListOf()
+       var tiles: MutableList<TileInfo> = createRandomTileInfo(2048, 2048).toMutableList()
 }
