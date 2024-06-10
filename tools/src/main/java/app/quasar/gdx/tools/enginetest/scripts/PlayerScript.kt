8a81fab55ab45d238c782e6eec67edf2a111211e
PhoenixShell <app.phoenixshell@gmail.com>
2024-06-10 18:19:08 +1000

    Update PlayerScript.kt

--- tools/src/main/java/app/quasar/gdx/tools/enginetest/scripts/PlayerScript.kt
+++ tools/src/main/java/app/quasar/gdx/tools/enginetest/scripts/PlayerScript.kt
@@ -25,9 +25,9 @@ class PlayerScript: VectorNode<PlayerData>(), WorldPosition {
         data.rotation += clock.mulDeltaTime(data.rotateSpeed)
 
         if(Gdx.input.isKeyJustPressed(Keys.TAB)) {
-            self.setDimension(EngineTestWorld.DessertDimen)
             context.engine.setDimension(EngineTestWorld.DessertDimen)
             context.engine.createNode(selfDimension, DimensionTransition::class)
+            self.setDimension(EngineTestWorld.DessertDimen)
         }
 
         //TODO: fix issue that calling self.setDimensionAfter setting engine world means that this entity wont be drawn
